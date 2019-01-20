28e5f31bcbc0e93604f35a2148030a0e1bd3a6f0
Tatiana Tkachenko <tkachenko.tatiana16@gmail.com>
2019-01-20 18:32:43 +0200

    Add ci

--- server/ormconfig.js
+++ server/ormconfig.js
@@ -3,12 +3,12 @@ const dotenv = require("dotenv")
 
 dotenv.config({ path: path.resolve(__dirname, "..", ".env") });
 
-if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL must be provided");
-}
-
 if (process.env.NODE_ENV === 'test' && !process.env.TEST_DATABASE_URL) {
   throw new Error("TEST_DATABASE_URL must be provided");
+} 
+
+if (process.env.NODE_ENV !== 'test' && !process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL must be provided");
 }
 
 const dbURl = process.env.NODE_ENV === 'test' ? process.env.TEST_DATABASE_URL : process.env.DATABASE_URL

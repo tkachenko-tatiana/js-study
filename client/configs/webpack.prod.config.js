1085b34a9cac514c481cf0dd4da9e4ed1e4efbcd
yuriycherenkov <cherenkov.yuriy111@gmail.com>
2019-03-10 22:04:44 +0200

    fix build

--- client/configs/webpack.prod.config.js
+++ client/configs/webpack.prod.config.js
@@ -1,12 +1,8 @@
-const CleanWebpackPlugin = require('clean-webpack-plugin');
 const merge = require('webpack-merge');
 const baseConfig = require('./webpack.base.config');
 
 const prodConfig = {
     mode: 'production',
-    plugins: [
-      new CleanWebpackPlugin(['dist']),
-    ],
     performance: false,
 };
 
